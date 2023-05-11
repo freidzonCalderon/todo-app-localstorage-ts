97331b59c6b0472d51560e274e60ef98c0bec5ab
Freidzon Calderon <102637227+freidzonCalderon@users.noreply.github.com>
2023-05-11 12:28:22 -0600

    input setup

--- src/Components/InputForm.tsx
+++ src/Components/InputForm.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 
 const InputForm = () => {
+	const [inputTask, setInputTask] = useState<String>("");
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(inputTask);
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center">
-			<form className="w-full max-w-sm">
+			<form onSubmit={handleSubmit} className="w-full max-w-sm">
 				<div className="flex items-center border-b-2 border-teal-500 py-2">
 					<input
 						className="appearance-none bg-transparent border-none w-full text-gray-200 mr-3 py-1 px-2 leading-tight focus:outline-none"
 						type="text"
 						placeholder="Type a task"
 						required
+						onChange={(e) => {
+							setInputTask(e.target.value);
+						}}
 					/>
 					<button
 						className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-						type="button"
+						type="submit"
 						title="Add"
 					>
 						<FaPlus />
