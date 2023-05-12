a4251efabb4681a8c14022c7077ce8622fd7d51d
Freidzon Calderon <102637227+freidzonCalderon@users.noreply.github.com>
2023-05-12 13:30:15 -0600

    corrected edit tasks

--- src/Components/InputForm.tsx
+++ src/Components/InputForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaPlus } from "react-icons/fa";
 import { useInputContext } from "../Context/InputTaskContext";
 import { useTaskContext } from "../Context/ListTaskContext";
