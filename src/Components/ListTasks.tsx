bf92709f2577ec0bd3cb49719c5664949b570082
Freidzon Calderon <102637227+freidzonCalderon@users.noreply.github.com>
2023-05-12 11:54:00 -0600

    corrected edit tasks

--- src/Components/ListTasks.tsx
+++ src/Components/ListTasks.tsx
@@ -6,13 +6,13 @@ const ListTasks = () => {
 	const { tasks } = useTaskContext();
 
 	return (
-		<React.Fragment>
+		<>
 			{tasks.map((task) => (
 				<li key={task.id} className="list-none">
 					<TodoItem task={task} />
 				</li>
 			))}
-		</React.Fragment>
+		</>
 	);
 };
 
